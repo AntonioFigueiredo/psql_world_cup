368188e3636dfdc76545f092326949831eb0e102
Antonio Figueiredo <antonio_figueiredo@web.de>
2023-12-12 19:50:42 +0100

    Added files needed for submition

---
+++ insert_data.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $WINNER != "winner" ]]
+  then
+    # get team_id for winner
+    TEAM_NAME=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    # if not found
+    if [[ -z $TEAM_NAME ]]
+    then
+      # insert winner
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $WINNER
+      fi
+    fi
+
+    # get team_id for opponent
+    TEAM_NAME=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    # if not found
+    if [[ -z $TEAM_NAME ]]
+    then
+      # insert opponent
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $OPPONENT
+      fi
+    fi
+  fi
+done
+
+
+
